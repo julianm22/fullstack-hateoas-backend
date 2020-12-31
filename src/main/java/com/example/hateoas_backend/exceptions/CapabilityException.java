efa059c92621d71d9c62bf4b508672281639c5c1
Julian-Murillo <jomsk822@gmail.com>
2020-12-31 17:30:17 -0500

    adds custom exception for exception handling

---
+++ src/main/java/com/example/hateoas_backend/exceptions/CapabilityException.java
@@ -0,0 +1,9 @@
+package com.example.hateoas_backend.exceptions;
+
+public class CapabilityException extends RuntimeException {
+
+    public CapabilityException(String message) {
+        super(message);
+    }
+
+}
